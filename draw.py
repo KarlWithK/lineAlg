27c630f5bf519a3df238ca8d6ab39907ba25f3ce
Karl <jocelinc60@outlook.com>
2020-02-11 08:24:51 -0500

    summed up draw.py

--- draw.py
+++ draw.py
@@ -78,3 +78,60 @@ def octant8(x0, y0, x1, y1, screen, color):
             d -= B
         x0 += 1
         d += A
+
+
+def draw_line_one(x0, y0, x1, y1, screen, color):
+    if (x0 > x1):
+        x0, y0, x1, y1 = x1, y1, x0, y0
+    try:
+        slope = (y1 - y0) / (x1 - x0)
+        A = y1 - y0
+        B = -1 * (x1 - x0)
+        if (0 <= slope < 1):
+            d = (2 * A) - B
+            A *= 2
+            B *= 2
+            while (x0 <= x1):
+                plot(screen, color, int(x0), int(y0))
+                if (d > 0):
+                    y0 += 1
+                    d += B
+                x0 += 1
+                d += A
+        elif (slope >= 1):
+            d = A + (2 * B)
+            A *= 2
+            B *= 2
+            while (y0 <= y1):
+                plot(screen, color, int(x0), int(y0))
+                if (d < 0):
+                    x0 += 1
+                    d += A
+                y0 += 1
+                d += B
+        elif (slope <= -1):
+            d = A - (2 * B)
+            A *= 2
+            B *= 2
+            while (y0 >= y1):
+                plot(screen, color, int(x0), int(y0))
+                if (d > 0):
+                    x0 += 1
+                    d += A
+                y0 -= 1
+                d -= B
+        else:
+            d = (2 * A) + B
+            A *= 2
+            B *= 2
+            while (x0 <= x1):
+                plot(screen, color, int(x0), int(y0))
+                if (d < 0):
+                    y0 -= 1
+                    d -= B
+                x0 += 1
+            d += A
+    except ZeroDivisionError:
+        while (y0 <= y1):
+            plot(screen, color, int(x0), int(y0))
+            y0 += 1
